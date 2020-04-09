1369953eec57b728f32de8ce42b47b5f129fdb14
saransh-mehta <mehtasaransh614@gmail.com>
2020-04-09 13:28:54 +0000

    Adding allTasksDataset and Batcher

---
+++ data_manager.py
@@ -0,0 +1,106 @@
+'''
+Script to manage datasets for multiple tasks
+'''
+from torch.utils.data import Dataset, DataLoader, BatchSampler
+
+class allTasksDataset(Dataset):
+    '''
+    class to make pytorch dataset of the processed data for a specific task
+    taskDict :- list of dictionaries. Each dictioanry belong to the details of a 
+                dataset to be created for a task
+                [ {"data_task_id" : "", "data_path" : "", "data_task_type" : ""},
+                 ...]
+    '''
+    def __init_(self, taskDict):
+        self.taskDict = taskDict
+        self.allTasksData, self.taskIdTypeMap = self.make_all_datasets()
+
+    def read_data(self, readPath):
+        with open(readPath, 'r', encoding = 'utf-8') as file:
+            taskData = []
+            for i, line in enumerate(file):
+                sample = json.loads(line)
+                taskData.append(sample)
+        return taskData
+
+    def make_all_datasets(self):
+        '''
+        For each dataset entry in the taskDict, this function makes them into corresponding dataset 
+        and returns a dictionary mapping like {<task_id> : <dataset>,}
+        '''
+        allTasksData = {}
+        taskIdTypeMap = {} # mapping from task id to task type
+        for task in self.taskDict:
+            data = self.read_data(task["data_path"])
+            allTasksData[task["data_task_id"]] = data
+            taskIdTypeMap[task["data_task_id"]] = task["data_task_type"]
+
+            print('Read Data for Task Id: {}. Samples {}'.format(task["data_task_id"], len(data)))
+        return allTasksData, taskIdTypeMap
+
+    # some standard functions which need to be overridden from Dataset
+    #class for item, len etc..
+    def __len__(self):
+        return len(self.allTasksData)
+
+    # get item will be used to fetch a sample when required for the corresponding task id. 
+    def __getitem__(self, idx):
+        taskId, sampleId = idx
+        out = {"task": {"task_id": taskId, "task_type": self.taskIdTypeMap[taskId]},
+                "sample": self.allTasksData[taskId][idx]}
+        return out
+
+class Batcher(BatchSampler):
+    def __init__(self, dataObj, batchSize, shuffleTask = True, shuffleBatch = True, seed = 42):
+        '''
+        dataObj :- An instance of allTasksDataset containing data for all tasks
+        '''
+        self.dataObj = dataObj
+        self.allTasksData = dataObj.allTasksData
+        self.batchSize = batchSize
+        # to shuffle the indices in a batch
+        self.shuffleBatch = shuffleBatch
+        # to shuffle the samples picked up among all the tasks
+        self.shuffleTask = shuffleTask
+        self.seed = seed
+        
+        self.allTasksDataBatchIdxs = []
+        self.taskIdxId = []
+        for taskId, data in self.allTasksData.items():
+            self.allTasksDataBatchIdxs.append(make_batches(len(data)))
+            self.taskIdxId.append(taskId)
+
+    def make_batches(self, dataSize):
+        batchIdxs = [list(range(i, min(i+self.batchSize, dataSize))) for i in range(0, dataSize, self.batchSize)]
+        if self.shuffleBatch:
+            random.seed(self.seed)
+            random.shuffle(batchIdxs)
+        return batchIdxs
+
+    def make_task_idxs(self):
+        '''
+        This fn makes task indices for which a corresponding batch is created
+        eg. [0, 0, 1, 3, 0, 2, 3, 1, 1, ..] if task ids are 0,1,2,3
+        '''
+        taskIdxs = []
+        for i in range(len(self.allTasksDataBatchIdxs)):
+            taskIdxs += [i]*len(self.allTasksDataBatchIdxs[i])
+        if self.shuffleTask:
+            random.seed(self.seed)
+            random.shuffle(taskIdxs)
+        return taskIdxs
+
+    #over riding BatchSampler functions to generate iterators for all tasks
+    # and iterate
+    def __len__(self):
+        return sum(len(data) for taskId, data in self.allTasksData.items())
+
+    def __iter__(self):
+        allTasksIters = [iter(item) for item in self.allTasksDataBatchIdxs]
+        #all_iters = [iter(item) for item in self._train_data_list]
+        allIdxs = self.make_task_idxs()
+        for taskIdx in allIdxs:
+            # this batch belongs to a specific task id
+            batchTaskId = self.taskIdxId[taskIdx]
+            batch = next(allTasksIters[taskIdx])
+            yield [(batchTaskId, sampleIdx) for sampleIdx in batch]
